ea708ac608e6d152aae373587bc80a0b898fe8cd
ssstakahashi <shouhei0123456789@gmail.com>
2021-12-29 10:48:27 +0900

    ver.0.0.4

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,15 +2,16 @@
 // import "firebase/firestore";
 // import "firebase/storage";
 import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { firebaseConfig } from './env';
 
-const firebaseApp = initializeApp({ /* config */ });
-const auth = getAuth();
-onAuthStateChanged(auth, user => { console.log(user); });
+const firebase = initializeApp(firebaseConfig);
+export const auth = getAuth(firebase);
+export const db = getFirestore(firebase);
 
+// onAuthStateChanged(auth, user => { console.log(user); });
 
-export const auth = firebase.auth();
-export const db = firebase.firestore();
-export const storage = firebase.storage();
-export const functions = firebase.functions();
+// export const storage = firebase.storage();
+// export const functions = firebase.functions();
 // export const firebaseTimestamp = firebase.firestore.Timestamp;
